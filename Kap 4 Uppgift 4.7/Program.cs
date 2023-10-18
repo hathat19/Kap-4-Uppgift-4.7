4f3efe996d63dca66585ae746acc0d244d930b79
humy06001 <humy06001@E-PW0234KS>
2023-10-18 21:57:53 +0200

    Add project files.

---
+++ Kap 4 Uppgift 4.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace upg4_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett meddelande som ska skrivas ut vertikalt:");
+            string userSent = Console.ReadLine();
+
+            Console.WriteLine("Hur många mellanslag ifrån kanten vill du ha det skrivet?");
+            int spaces = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < userSent.Length; i++)
+            {
+                if (spaces > 0)
+                {
+                    for (int j = 1; j <= spaces; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                
+                Console.WriteLine(userSent[i]);
+            }
+        }
+    }
+}
